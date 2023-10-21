aba6fc1d19e91d5b3471089a490d0513a419a12f
sanaullah129 <109277244+sanaullah129@users.noreply.github.com>
2023-10-21 21:33:07 +0530

    Added delete palette feature. File locations updated

--- src/App.js
+++ src/App.js
@@ -1,44 +1,59 @@
 import React, { Component } from "react";
 import { Route, Switch } from "react-router-dom";
-import Palette from "./components/Palette";
+import Palette from './components/Palette';
 import PaletteList from "./components/PaletteList";
 import SingleColorPalette from "./components/SingleColorPalette";
-import seedColors from "./components/seedColors";
-import { generatePalette } from './components/colorHelpers'
+import seedColors from "./constants/seedColors";
 import NewPaletteForm from "./components/NewPaletteForm";
+import { generatePalette } from "./constants/colorHelpers";
 
 class App extends Component {
-
   constructor(props) {
     super(props);
     const savedPalettes = JSON.parse(window.localStorage.getItem("palettes"));
     this.state = { palettes: savedPalettes || seedColors };
     this.savePalette = this.savePalette.bind(this);
     this.findPalette = this.findPalette.bind(this);
+    this.deletePalette = this.deletePalette.bind(this);
   }
-
   findPalette(id) {
-    return this.state.palettes.find(function (palette) {
+    return this.state.palettes.find(function(palette) {
       return palette.id === id;
     });
-  };
-
+  }
+  deletePalette(id) {
+    this.setState(
+      st => ({ palettes: st.palettes.filter(palette => palette.id !== id) }),
+      this.syncLocalStorage
+    );
+  }
   savePalette(newPalette) {
-    this.setState({ palettes: [...this.state.palettes, newPalette] },
-      this.syncLocalStorage);
-  };
-
+    this.setState(
+      { palettes: [...this.state.palettes, newPalette] },
+      this.syncLocalStorage
+    );
+  }
   syncLocalStorage() {
+    //save palettes to local storage
     window.localStorage.setItem(
-      "palettes", 
+      "palettes",
       JSON.stringify(this.state.palettes)
-      );
-  };
-
+    );
+  }
   render() {
     return (
       <Switch>
-        <Route exact path='/palette/new' render={(routeProps) => <NewPaletteForm savePalette={this.savePalette} palettes={this.state.palettes} {...routeProps} />} />
+        <Route
+          exact
+          path='/palette/new'
+          render={routeProps => (
+            <NewPaletteForm
+              savePalette={this.savePalette}
+              palettes={this.state.palettes}
+              {...routeProps}
+            />
+          )}
+        />
         <Route
           exact
           path='/palette/:paletteId/:colorId'
@@ -55,7 +70,11 @@ class App extends Component {
           exact
           path='/'
           render={routeProps => (
-            <PaletteList palettes={this.state.palettes} {...routeProps} />
+            <PaletteList
+              palettes={this.state.palettes}
+              deletePalette={this.deletePalette}
+              {...routeProps}
+            />
           )}
         />
         <Route
--- src/components/PaletteList.js
+++ src/components/PaletteList.js
@@ -1,27 +1,30 @@
 import React, { Component } from "react";
+import { Link } from "react-router-dom";
 import MiniPalette from "./MiniPalette";
 import { withStyles } from "@material-ui/styles";
 import styles from "../styles/PaletteListStyles";
-import { Link } from "react-router-dom";
 
 class PaletteList extends Component {
   goToPalette(id) {
     this.props.history.push(`/palette/${id}`);
   }
   render() {
-    const { palettes, classes } = this.props;
+    const { palettes, classes, deletePalette } = this.props;
     return (
       <div className={classes.root}>
         <div className={classes.container}>
           <nav className={classes.nav}>
-            <h2>Palette Plus</h2>
-            <Link to="/palette/new">Create a new palette</Link>
+            <h1>Palette Plus</h1>
+            <Link to='/palette/new'>Create Palette</Link>
           </nav>
           <div className={classes.palettes}>
             {palettes.map(palette => (
               <MiniPalette
                 {...palette}
                 handleClick={() => this.goToPalette(palette.id)}
+                handleDelete={deletePalette}
+                key={palette.id}
+                id={palette.id}
               />
             ))}
           </div>
